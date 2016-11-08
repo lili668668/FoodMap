7b4a29f9ed26f5eaed0ad164fc8665f18fc02dd9
Tzu-Yin Hong <lili668668@gmail.com>
2016-11-08 09:24:34 +0800

    完成資料庫的類別，可是我不想寫註解OAO

--- app/src/main/java/ballfish/util/sqlite/Database.java
+++ app/src/main/java/ballfish/util/sqlite/Database.java
@@ -2,7 +2,11 @@ package ballfish.util.sqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.sql.Blob;
+import java.util.ArrayList;
 
 /**
  * 資料庫的物件，用以操作資料庫使用
@@ -13,6 +17,14 @@ public class Database {
     private int version = 1;
     private DbOpenHelper dbHelper = null;
     private SQLiteDatabase db = null;
+
+    public static final String LIKE = " like ?";
+    public static final String EQUALS = " = ?";
+    public static final String MORE = " > ?";
+    public static final String LESS = " < ?";
+    public static final String NOTEQUALS = " != ?";
+    public static final String AND = " and ";
+    public static final String OR = " or ";
 
     /**
      * 連線資料庫，若該資料庫不存在，則會建立該資料庫
@@ -59,8 +71,64 @@ public class Database {
         return res;
     }
 
-    public long UpdateRow(String tableName, ContentValues row) {
-        return 0;
+    public long UpdateRow(String tableName, ContentValues row, Column column, String where, String[] clause) {
+        int res = this.db.update(tableName,row, column.name + where, clause);
+        return res;
+    }
+
+    public long DeleteRow(String tableName, Column column, String where, String[] clause) {
+        long res = this.db.delete(tableName, column.name + where,clause);
+        return res;
+    }
+
+    public ContentValues[] SelectRow(String tableName, Column column, String where, String[] clause, Column orderby) {
+        ArrayList<ContentValues> rows = new ArrayList<ContentValues>();
+        Cursor cursor = null;
+        try {
+            cursor = db.query(tableName, null, column.name + where, clause, null, null, orderby.name);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+
+                ContentValues v = new ContentValues();
+                int index = cursor.getPosition();
+                int type = cursor.getType(index);
+                switch (type) {
+                    case Cursor.FIELD_TYPE_INTEGER:
+                        String name = cursor.getColumnName(index);
+                        int integer = cursor.getInt(index);
+                        v.put(name, integer);
+                        break;
+                    case Cursor.FIELD_TYPE_FLOAT:
+                        name = cursor.getColumnName(index);
+                        float fl = cursor.getFloat(index);
+                        v.put(name, (double)fl);
+                        break;
+                    case Cursor.FIELD_TYPE_STRING:
+                        name = cursor.getColumnName(index);
+                        String string = cursor.getString(index);
+                        v.put(name, string);
+                        break;
+                    case Cursor.FIELD_TYPE_BLOB:
+                        name = cursor.getColumnName(index);
+                        byte[] blob = cursor.getBlob(index);
+                        v.put(name, blob);
+                        break;
+                }
+                if (v != null) {
+                    rows.add(v);
+                }
+            }
+        }
+        ContentValues[] cv = null;
+        cv = rows.toArray(cv);
+
+        return cv;
     }
 
 
