be7610df326b7f9c7827a455e59f1860e72c5c3f
Tzu-Yin Hong <lili668668@gmail.com>
2016-11-20 11:19:03 +0800

    標註現在位置

--- app/src/main/java/scu/csim/student/foodmap/MainActivity.java
+++ app/src/main/java/scu/csim/student/foodmap/MainActivity.java
@@ -133,10 +133,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        ((MyLocationListener) locationListener).setMap(googleMap);
     }
 
     private void openGPS() {
--- app/src/main/java/scu/csim/student/foodmap/MyLocationListener.java
+++ app/src/main/java/scu/csim/student/foodmap/MyLocationListener.java
@@ -5,12 +5,18 @@ import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * GPS實作
  */
 
 public class MyLocationListener implements android.location.LocationListener {
-    Context context;
+    private Context context;
+    private GoogleMap map = null;
 
     public MyLocationListener(Context context) {
         this.context = context;
@@ -18,6 +24,15 @@ public class MyLocationListener implements android.location.LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
+        double latittude = location.getLatitude();
+        double longitude = location.getLongitude();
+        if (map != null) {
+            map.clear();
+            LatLng nowLat = new LatLng(latittude, longitude);
+            map.addMarker(new MarkerOptions().position(nowLat).title("Now Location"));
+            map.moveCamera(CameraUpdateFactory.newLatLng(nowLat));
+        }
+
         String str = "緯度" + location.getLatitude() + " 經度" + location.getLongitude() + " 標高" + location.getAltitude() + " 方位" + location.getBearing();
         Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
     }
@@ -35,5 +50,9 @@ public class MyLocationListener implements android.location.LocationListener {
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    public void setMap(GoogleMap map) {
+        this.map = map;
     }
 }
