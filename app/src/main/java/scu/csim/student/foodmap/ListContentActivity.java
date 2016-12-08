6f2d3c62805526e8e3918d2f9a34f68185b6a928
Tzu-Yin Hong <lili668668@gmail.com>
2016-12-08 22:24:40 +0800

    動態加入圖片

--- app/src/main/java/scu/csim/student/foodmap/ListContentActivity.java
+++ app/src/main/java/scu/csim/student/foodmap/ListContentActivity.java
@@ -3,7 +3,10 @@ package scu.csim.student.foodmap;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -15,7 +18,7 @@ public class ListContentActivity extends AppCompatActivity {
 
     Context context;
 
-    ImageView restPhoto;
+    LinearLayout PhotosList;
     TextView restTitle;
     TextView resdetail;
     TextView resaddress;
@@ -27,7 +30,8 @@ public class ListContentActivity extends AppCompatActivity {
 
         context = getApplicationContext();
 
-        restPhoto = (ImageView) findViewById(R.id.food_imageview);
+        PhotosList = (LinearLayout) findViewById(R.id.photos_view);
+
         restTitle = (TextView) findViewById(R.id.restTitle_textView);
         resdetail = (TextView) findViewById(R.id.detail_textView);
         resaddress = (TextView) findViewById(R.id.address_textView);
@@ -44,11 +48,22 @@ public class ListContentActivity extends AppCompatActivity {
             // 很多圖片的檔名是用逗號分隔的
             String[] rest_photos_arr = rest_photos.split(",");
 
-            // 圖片的網址
-            String url = RestaurantAPI.HOST + RestaurantAPI.PHOTO_DIR + rest_photos_arr[0];
+            // 迴圈跑每個圖片檔案
+            for(int cnt = 0;cnt < rest_photos_arr.length;cnt++) {
+                // 新增一個ImageView
+                ImageView im = new ImageView(context);
+                PhotosList.addView(im);
 
-            // 下載圖片並且傳送給ImageView
-            Picasso.with(context).load(url).into(restPhoto);
+                // 圖片的網址
+                String url = RestaurantAPI.HOST + RestaurantAPI.PHOTO_DIR + rest_photos_arr[cnt];
+
+                // 下載圖片並且傳送給ImageView
+                Picasso.with(context)
+                        .load(url)
+                        .placeholder(R.drawable.pic_no_pix)
+                        .error(R.drawable.pic_no_pix)
+                        .into(im);
+            }
         }
 
         restTitle.setText(rest_name);
